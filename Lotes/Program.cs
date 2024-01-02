f7ac69a61616313700ff64f70310310ee2af55c9
jorge.chaparro <jorge.chaparro@FG-NB-3000>
2024-01-02 16:05:40 -0300

    funcion entrada mercancias compras

--- Lotes/Program.cs
+++ Lotes/Program.cs
@@ -6,8 +6,11 @@ using System.Windows.Forms;
 
 namespace Lotes
 {
+    
     class Program
     {
+        public static string v_LoteNom = "";
+        public static int v_num = 0;
         public static SAPbouiCOM.EditText v_codigo = null;
         /// <summary>
         /// The main entry point for the application.
@@ -51,12 +54,16 @@ namespace Lotes
                 {
                     //agarramos el formulario
                     SAPbouiCOM.Form form = SAPbouiCOM.Framework.Application.SBO_Application.Forms.Item(FormUID);
-                    //consultamos la descripcion
-                    SAPbobsCOM.Recordset oNumLote;
-                    oNumLote = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
-                    oNumLote.DoQuery("SELECT \"Name\" FROM \"@LOTENUM\" ");
-                    string v_LoteNom = "LT";// oNumLote.Fields.Item(0).Value.ToString();
                     
+
+                    if(pVal.EventType == BoEventTypes.et_FORM_LOAD && pVal.BeforeAction == false){
+                        //consultamos la descripcion
+                        SAPbobsCOM.Recordset oNumLote;
+                        oNumLote = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+                        oNumLote.DoQuery("SELECT \"Name\",\"U_LoteNum\" FROM \"@LOTENUM\" ");
+                        v_LoteNom = oNumLote.Fields.Item(0).Value.ToString();   
+                        v_num = int.Parse(oNumLote.Fields.Item(1).Value.ToString());
+                    }
                     //evento al presionar tabulador
                     #region TABULADOR
                     if (pVal.EventType == BoEventTypes.et_LOST_FOCUS && pVal.BeforeAction==false && pVal.ItemUID == "3" && pVal.ColUID=="2") 
@@ -64,35 +71,38 @@ namespace Lotes
                         SAPbouiCOM.Matrix oMatrix = (SAPbouiCOM.Matrix)form.Items.Item("3").Specific;
                         SAPbouiCOM.EditText v_texto = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row).Specific;
                         string texto_v = v_texto.Value;            
-                        if((!texto_v.Contains("LT") || string.IsNullOrEmpty(texto_v)))//if (pVal.CharPressed == (char)9 && pVal.ColUID=="2" )
+                        if((string.IsNullOrEmpty(texto_v)))//if (pVal.CharPressed == (char)9 && pVal.ColUID=="2" )
                         {
-                            
-                            if (pVal.Row > 1)
-                            {
-                                SAPbouiCOM.EditText oLoteNumer = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row-1).Specific;
-                                string v_numeroLote = oLoteNumer.Value;
-                                v_numeroLote = v_numeroLote.Replace(v_LoteNom, string.Empty);
-                                int v_nuevoLote = int.Parse(v_numeroLote) + 1;
-                                v_numeroLote = v_LoteNom + v_nuevoLote.ToString();
+                            SAPbouiCOM.EditText oLote = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row).Specific;
+                            v_num = v_num + 1;
+                            oLote.Value = v_LoteNom + v_num;
+                            oMatrix.Columns.Item("3").Cells.Item(pVal.Row).Click();
+                            //if (pVal.Row > 1)
+                            //{
+                            //    SAPbouiCOM.EditText oLoteNumer = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row-1).Specific;
+                            //    string v_numeroLote = oLoteNumer.Value;
+                            //    v_numeroLote = v_numeroLote.Replace(v_LoteNom, string.Empty);
+                            //    int v_nuevoLote = v_num + 1;//int.Parse(v_numeroLote) + 1;
+                            //    v_numeroLote = v_LoteNom + v_nuevoLote.ToString();
 
-                                SAPbouiCOM.EditText oLoteNuevo = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row).Specific;
-                                oLoteNuevo.Value = v_numeroLote;
-                                oMatrix.Columns.Item("3").Cells.Item(pVal.Row).Click();
-                            }
-                            else
-                            {
-                                //agarramos los datos del formulario                           
-                                SAPbouiCOM.EditText oLote = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row).Specific;
-                                SAPbouiCOM.EditText oCan = (SAPbouiCOM.EditText)oMatrix.Columns.Item("3").Cells.Item(pVal.Row).Specific;
-                                //realizamos una consulta a la tabla
-                                SAPbobsCOM.Recordset oNumLote1;
-                                oNumLote1 = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
-                                oNumLote1.DoQuery("SELECT \"U_LoteNum\"+1 FROM \"@LOTENUM\" ");
-                                string v_LoteNum = oNumLote1.Fields.Item(0).Value.ToString();
-                                //mandamos el numero de lote a la tabla
-                                oLote.Value = v_LoteNom + v_LoteNum;
-                                oMatrix.Columns.Item("3").Cells.Item(pVal.Row).Click();
-                            }
+                            //    SAPbouiCOM.EditText oLoteNuevo = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row).Specific;
+                            //    oLoteNuevo.Value = v_numeroLote;
+                            //    oMatrix.Columns.Item("3").Cells.Item(pVal.Row).Click();
+                            //}
+                            //else
+                            //{
+                            //    //agarramos los datos del formulario                           
+                            //    SAPbouiCOM.EditText oLote = (SAPbouiCOM.EditText)oMatrix.Columns.Item("2").Cells.Item(pVal.Row).Specific;
+                            //    SAPbouiCOM.EditText oCan = (SAPbouiCOM.EditText)oMatrix.Columns.Item("3").Cells.Item(pVal.Row).Specific;
+                            //    //realizamos una consulta a la tabla
+                            //    SAPbobsCOM.Recordset oNumLote1;
+                            //    oNumLote1 = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+                            //    oNumLote1.DoQuery("SELECT \"U_LoteNum\"+1 FROM \"@LOTENUM\" ");
+                            //    string v_LoteNum = oNumLote1.Fields.Item(0).Value.ToString();
+                            //    //mandamos el numero de lote a la tabla
+                            //    oLote.Value = v_LoteNom + v_num;
+                            //    oMatrix.Columns.Item("3").Cells.Item(pVal.Row).Click();
+                            //}
                            
                         }
                     }
@@ -101,7 +111,7 @@ namespace Lotes
                 #endregion
 
                 #region FORM ENTRADA DE MERCADERIA
-                if (pVal.FormTypeEx == "143")
+                if (pVal.FormTypeEx == "721")
                 {
                     //agarramos el formulario
                     SAPbouiCOM.Form form = SAPbouiCOM.Framework.Application.SBO_Application.Forms.Item(FormUID);
@@ -116,40 +126,65 @@ namespace Lotes
                     if(pVal.ItemUID=="1" && pVal.FormMode==3 && pVal.BeforeAction==false && pVal.ActionSuccess && pVal.EventType==BoEventTypes.et_ITEM_PRESSED)
                     {
                         //realizamos una consulta a la tabla
-                        SAPbobsCOM.Recordset oNumLote1;
-                        oNumLote1 = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
-                        oNumLote1.DoQuery("SELECT \"U_LoteNum\"+1, \"Name\" FROM \"@LOTENUM\" ");
-                        string v_LoteNum = oNumLote1.Fields.Item(0).Value.ToString();
-                        int v_lote = int.Parse(v_LoteNum);
-                        string v_nombre = oNumLote1.Fields.Item(1).Value.ToString();
+                        //SAPbobsCOM.Recordset oNumLote1;
+                        //oNumLote1 = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+                        //oNumLote1.DoQuery("SELECT \"U_LoteNum\"+1, \"Name\" FROM \"@LOTENUM\" ");
+                        //string v_LoteNum = oNumLote1.Fields.Item(0).Value.ToString();
+                        //int v_lote = int.Parse(v_LoteNum);
+                        //string v_nombre = oNumLote1.Fields.Item(1).Value.ToString();
 
-                        //consultamos los datos a actualizar
-                        SAPbobsCOM.Recordset oDatos;
-                        oDatos = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
-                        oDatos.DoQuery("SELECT \"ItemCode\",\"AbsEntry\",\"DistNumber\" FROM OBTN WHERE \"DistNumber\" Like 'LT%' ORDER BY \"AbsEntry\" ASC");
-                        //recorremos
-                        while (!oDatos.EoF)
-                        {
-                            string v_entry = oDatos.Fields.Item(1).Value.ToString();
-                            //el nombre del nuevo lote
-                            string v_lotenew = v_nombre + v_lote.ToString();
-                            //actualizamos los lotes nuevos
-                            SAPbobsCOM.Recordset oUpdate;
-                            oUpdate = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
-                            oUpdate.DoQuery("UPDATE OBTN SET \"DistNumber\"='"+v_lotenew+ "' WHERE \"AbsEntry\"='"+v_entry+"' ");
-                            oDatos.MoveNext();
-                            v_lote++;
-                            v_lotenew = "";
-                        }
+                        ////consultamos los datos a actualizar
+                        //SAPbobsCOM.Recordset oDatos;
+                        //oDatos = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+                        //oDatos.DoQuery("SELECT \"ItemCode\",\"AbsEntry\",\"DistNumber\" FROM OBTN WHERE \"DistNumber\" Like 'LT%' ORDER BY \"AbsEntry\" ASC");
+                        ////recorremos
+                        //while (!oDatos.EoF)
+                        //{
+                        //    string v_entry = oDatos.Fields.Item(1).Value.ToString();
+                        //    //el nombre del nuevo lote
+                        //    string v_lotenew = v_nombre + v_lote.ToString();
+                        //    //actualizamos los lotes nuevos
+                        //    SAPbobsCOM.Recordset oUpdate;
+                        //    oUpdate = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+                        //    oUpdate.DoQuery("UPDATE OBTN SET \"DistNumber\"='"+v_lotenew+ "' WHERE \"AbsEntry\"='"+v_entry+"' ");
+                        //    oDatos.MoveNext();
+                        //    v_lote++;
+                        //    v_lotenew = "";
+                        //}
                         //actualizamos la tabla de lote
-                        v_lote = v_lote - 1;
+                        //v_lote = v_lote - 1;
                         SAPbobsCOM.Recordset oUpdateLote;
                         oUpdateLote = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
-                        oUpdateLote.DoQuery("UPDATE \"@LOTENUM\" SET \"U_LoteNum\"='" + v_lote + "' WHERE \"Code\"='1' ");
+                        oUpdateLote.DoQuery("UPDATE \"@LOTENUM\" SET \"U_LoteNum\"='" + v_num + "' WHERE \"Code\"='1' ");
+                        v_num = 0;
 
                     }
                 }
                 #endregion
+
+                #region FORM COMPRAS ENTRADA DE MERCANCIAS
+                if (pVal.FormTypeEx == "143")
+                {
+                    //agarramos el formulario
+                    SAPbouiCOM.Form form = SAPbouiCOM.Framework.Application.SBO_Application.Forms.Item(FormUID);
+
+                    //evento antes de crear el documento
+                    if (pVal.ItemUID == "1" && pVal.BeforeAction == true && pVal.EventType == BoEventTypes.et_ITEM_PRESSED)
+                    {
+                        v_codigo = (SAPbouiCOM.EditText)form.Items.Item("8").Specific;
+                    }
+
+                    //evento después de crear el documento
+                    if (pVal.ItemUID == "1" && pVal.FormMode == 3 && pVal.BeforeAction == false && pVal.ActionSuccess && pVal.EventType == BoEventTypes.et_ITEM_PRESSED)
+                    {
+                        SAPbobsCOM.Recordset oUpdateLote;
+                        oUpdateLote = (SAPbobsCOM.Recordset)Menu.sbo.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset);
+                        oUpdateLote.DoQuery("UPDATE \"@LOTENUM\" SET \"U_LoteNum\"='" + v_num + "' WHERE \"Code\"='1' ");
+                        v_num = 0;
+                    }
+                }
+                #endregion
+
             }
             catch (Exception e)
             {
